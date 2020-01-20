6b6ef76184b15e0aa897e5a81d75405a872a6de0
Clayton Menezes <claytonmenezes@hotmail.com>
2020-01-20 17:46:47 -0300

    Primeiro Commit

---
+++ RoboExtracaoProcesso/RoboExtracaoProcesso/Classes/ResponseState.cs
@@ -0,0 +1,12 @@
+﻿namespace RoboExtracaoProcesso.Classes
+{
+    public enum ResponseState
+    {
+        OK,
+        TIMEOUT,
+        INVALID_REQUEST,
+        INCORRECT_ACCESS_KEY,
+        NOT_ENOUGH_FUND,
+        UNKNOWN
+    }
+}
---
+++ RoboExtracaoProcesso/RoboExtracaoProcesso/Form1.cs
@@ -0,0 +1,26 @@
+﻿using RoboExtracaoProcesso.Classes;
+using System.Windows.Forms;
+
+namespace RoboExtracaoProcesso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var selenium = new Selenium();
+            //var solver = new HumanCoder("http://api.captchaboss.com/Imagepost.ashx", "C44CPSGKH964LH534A64UXMQPMY7UKVJ9Z2LQZV3");
+
+            selenium.startBrowser();
+            selenium.PreencheNumeroProcesso();
+            var imgBase64 = selenium.downloadCaptcha();
+            //solver.SolveCaptcha(imgBase64);
+            //string CaptchaText = solver.LastResponseText;
+            selenium.closeBrowser();
+        }
+    }
+}
