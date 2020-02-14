19659e14ee4fe7b7b81f59b6edbc8c9e36a934c0
Clayton Menezes <claytonmenezes@hotmail.com>
2020-02-13 21:20:27 -0300

    melhorando o codigo

--- src/libs/selenium.js
+++ src/libs/selenium.js
@@ -1,16 +1,11 @@
 import {Builder, By, Key, util} from 'selenium-webdriver'
-// import utils from './utils.js'
-import htmlToImage from 'html-to-image'
+import utils from './utils.js'
 
 export default {
   async start () {
     let driver = await new Builder().forBrowser('chrome').build()
     await driver.get('https://sistemas.anm.gov.br/SCM/site/admin/dadosProcesso.aspx')
-    let el = driver.findElement(By.xpath('//*[@id="ctl00_conteudo_trCaptcha"]/td[2]/div[1]/span[1]/img'))
-    htmlToImage.toBlob(el).then(function (blob) {
-      console.log(blob)
-    });
-    // await utils.pegaCaptcha(driver, By, '//*[@id="ctl00_conteudo_trCaptcha"]/td[2]/div[1]/span[1]/img')
+    await utils.pegaCaptcha(driver, By, '//*[@id="ctl00_conteudo_trCaptcha"]/td[2]/div[1]/span[1]/img')
     // .then(resolve => {
     //     console.log(resolve)
     //     // driver.findElement(By.name('q')).sendKeys(resolve, Key.RETURN)
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ import selenium from '../libs/selenium'
 
 module.exports = app => {
   app.get('/buscar/:processos', async (req, res) => {
-    selenium.start()
-    res.send(selenium.start())
-    // res.send(req.params.processos)
+    res.send(selenium.start(req.params.processos))
   })
 }
